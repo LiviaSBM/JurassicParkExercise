53d409a29bdccd34a4c69551899ae9d1a9eb4652
Livia Menezes <liviasbm@hotmail.com>
2022-09-10 09:59:18 -0400

    Create JurassicPark.py
    Creation of initial structure of JP OPP exercise code

---
+++ JurassicPark.py
@@ -0,0 +1,43 @@
+#Student Name: Livia Menezes
+#POO Exercise - Luiza Code Python Bootcamp
+
+Print("Welcome to Jurassic Park interactive system\n")
+name = input("Please inform your name: \n")
+user = input("Please, indicate whether you are a Visitor or an Employee:\n")
+
+
+#Fazer um IF pra chamar o módulo de visitor ou de employee
+class MainMenu:
+    def __init__(self,name):
+        self.name = name
+
+    def meuemployee(self):
+        pass
+
+class Employee:
+    pass
+
+class Visitor:
+    pass
+
+class DinoCatalogue:
+
+    #print(The {dino, list[0]} is a {type, list[1]} from {period, list[2] period}. 
+    #It's a {eating} that used to live in {habitat}. {impacttext})
+    pass
+
+class Atractions:
+    #fazer catalogo com atracoes, com link com os dinosauros disponiveis em casa uma
+
+class DNALab:
+    #fazer classe privada
+    
+#Fazer lista com caracteristicas q serao importadas pela classe q dará a explicacao
+#Tyranossaurusrex
+#Velociraptor
+#Brachiosaurus
+#Triceratops
+#Dilophosaurus
+#Compsognathus
+#Gallimimus
+#Stegosaurus
