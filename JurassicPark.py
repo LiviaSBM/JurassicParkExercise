318f93786d1e778034423168ad43d143dc048c40
Livia Menezes <liviasbm@hotmail.com>
2022-09-10 14:23:09 -0400

    Update JurassicPark.py
    Some issues fixed with class VisitorsMenu - Not yet functional

--- JurassicPark.py
+++ JurassicPark.py
@@ -1,7 +1,7 @@
 #Student Name: Livia Menezes
 #POO Exercise - Luiza Code Python Bootcamp
 
-dinodictionary = {"T-Rex": ["theropod","Cretaceous","bipedal carnivore","North America","T. rex could grow to lengths of over 12.4 m (40.7 ft), up to 3.66–3.96 m (12–13 ft) tall at the hips, and 8.87 metric tons (9.78 short tons) in body mass.[2] Although other theropods rivaled or exceeded Tyrannosaurus rex in size, it is still among the largest known land predators and is estimated to have exerted the strongest bite force among all terrestrial animals."], "Velociraptor": ["theropod","Cretaceous","bipedal carnivore","North America","Smaller than other dromaeosaurids like Deinonychus and Achillobator, Velociraptor was about 1.5–2.07 m (4.9–6.8 ft) long with a body mass between 15–18.3 kg (33–40 lb), roughly the size of a turkey. However, it's believed to have been a smart dinosaur and have had good communication skills."], "Brachiosaurus": ["sauropod","late Jurassic","herbivorous","North America","Brachiosaurus is estimated to have been between 18 and 22 meters (59 and 72 ft) long. It had a disproportionately long neck, small skull, and large overall size, all of which are typical for sauropods. Atypically, Brachiosaurus had longer forelimbs than hindlimbs, which resulted in a steeply inclined trunk, and a proportionally shorter tail."], "Triceratops": ["ceratopsid","Late Cretaceous","herbivorous","North America","The name Triceratops, which literally means 'three-horned face'. Triceratops is one of the most recognizable of all dinosaurs and the most well-known ceratopsid. It was also one of the largest, up to 8–9 metres (26–30 ft) long and 5–9 metric tons (5.5–9.9 short tons) in body mass."], "Dilophosaurus": ["theropod","Early Jurassic","bipedal carnivore","North America","At about 7 m (23 ft) in length, with a weight of about 400 kg (880 lb), Dilophosaurus was one of the earliest large predatory dinosaurs and the largest known land-animal in North America at the time."], "Compsognathus": ["theropod","Jurassic","bipedal carnivore","Europe","Many presentations still describe Compsognathus as chicken-sized dinosaurs because of the size of the German specimen, which is now believed to be a juvenile. Compsognathus longipes is one of the few dinosaur species whose diet is known with certainty: the remains of small, agile lizards are preserved in the bellies of both specimens"], "Gallimimus": ["theropod","Late Creataceous","bipedal herbivorous","Asia","Gallimimus is the largest known ornithomimid; adults were about 6 metres (20 ft) long, 1.9 metres (6 ft 3 in) tall at the hip and weighed about 440–450 kilograms (970–990 lb)."], "Stegosaurus": ["armored dinosaur","Late Jurassic","herbivorous quadrupeds","Europe and North America","Due to their distinctive combination of broad, upright plates and tail tipped with spikes, Stegosaurus is one of the most recognizable kinds of dinosaurs. The function of this array of plates and spikes has been the subject of much speculation among scientists. Today, it is generally agreed that their spiked tails were most likely used for defense against predators, while their plates may have been used primarily for display, and secondarily for thermoregulatory functions."] }
+dinodictionary = {1: ["Tyrannosaurus Rex","theropod","Cretaceous","bipedal carnivore","North America","T. rex could grow to lengths of over 12.4 m (40.7 ft), up to 3.66–3.96 m (12–13 ft) tall at the hips, and 8.87 metric tons (9.78 short tons) in body mass.[2] Although other theropods rivaled or exceeded Tyrannosaurus rex in size, it is still among the largest known land predators and is estimated to have exerted the strongest bite force among all terrestrial animals."], 2: ["Velociraptor","theropod","Cretaceous","bipedal carnivore","North America","Smaller than other dromaeosaurids like Deinonychus and Achillobator, Velociraptor was about 1.5–2.07 m (4.9–6.8 ft) long with a body mass between 15–18.3 kg (33–40 lb), roughly the size of a turkey. However, it's believed to have been a smart dinosaur and have had good communication skills."], 3: ["Brachiosaurus","sauropod","late Jurassic","herbivorous","North America","Brachiosaurus is estimated to have been between 18 and 22 meters (59 and 72 ft) long. It had a disproportionately long neck, small skull, and large overall size, all of which are typical for sauropods. Atypically, Brachiosaurus had longer forelimbs than hindlimbs, which resulted in a steeply inclined trunk, and a proportionally shorter tail."], 4: ["Triceratops","ceratopsid","Late Cretaceous","herbivorous","North America","The name Triceratops, which literally means 'three-horned face'. Triceratops is one of the most recognizable of all dinosaurs and the most well-known ceratopsid. It was also one of the largest, up to 8–9 metres (26–30 ft) long and 5–9 metric tons (5.5–9.9 short tons) in body mass."], 5: ["Dilophosaurus","theropod","Early Jurassic","bipedal carnivore","North America","At about 7 m (23 ft) in length, with a weight of about 400 kg (880 lb), Dilophosaurus was one of the earliest large predatory dinosaurs and the largest known land-animal in North America at the time."], 6: ["Compsognathus","theropod","Jurassic","bipedal carnivore","Europe","Many presentations still describe Compsognathus as chicken-sized dinosaurs because of the size of the German specimen, which is now believed to be a juvenile. Compsognathus longipes is one of the few dinosaur species whose diet is known with certainty: the remains of small, agile lizards are preserved in the bellies of both specimens"], 7: ["Gallimimus","theropod","Late Creataceous","bipedal herbivorous","Asia","Gallimimus is the largest known ornithomimid; adults were about 6 metres (20 ft) long, 1.9 metres (6 ft 3 in) tall at the hip and weighed about 440–450 kilograms (970–990 lb)."], 8: ["Stegosaurus","armored dinosaur","Late Jurassic","herbivorous quadrupeds","Europe and North America","Due to their distinctive combination of broad, upright plates and tail tipped with spikes, Stegosaurus is one of the most recognizable kinds of dinosaurs. The function of this array of plates and spikes has been the subject of much speculation among scientists. Today, it is generally agreed that their spiked tails were most likely used for defense against predators, while their plates may have been used primarily for display, and secondarily for thermoregulatory functions."] }
 
 print("Welcome to Jurassic Park interactive system\n")
 name = input("Please inform your name: \n")
@@ -43,21 +43,38 @@ class MainMenu: #Pergunta qual item do Menu o usuário quer
 #print(execucao.execute())
 
 
-class VisitorsMenu: 
-    def __init__(self, choice, user):
+class VisitorsMenu: #OK- Encapsulamento
+    def __init__(self, choice):
         self.choice = choice
+        self.dinosaur = dinodictionary
         
     def visitor1_knowthepark(self):
-        pass
+        print("The park is divided in 4 main areas. The carnivores are located at the east and west zones, whereas the herbivores are in the North and South.\nThe Helicopter ride is located at the South shore.\nThe Port is at the East shore.\nThe Visitor center is at the West island.\nYou can get a map with all locations and attractions at the Visitor Center.\n")
+
+        askifrestart = Restart(user)
+        return askifrestart.restart()
 
     def visitor2_dinosaurcatalogue(self):
-        pass
+        species=int(input("Jurassic Park has currently 8 different species of dinosaurus.\nPlease choose the number below of the one you'd like to read about:\n1.T-Rex\n2.Velociraptor\n3.Brachiosaurus\n4.Triceratops\n5.Compsognatus\n6.Gallimimus\n7.Stegosaurus\n8.Stegosaurus\n"))
+
+        print(f"The {self.dinosaur[0]} is a {self.dinosaur[1]} from {self.dinosaur[2]} period. It's a {self.dinosaur[3]} that used to live in {self.dinosaur[4]}. {self.dinosaur[5]}")
+
+        askifrestart = Restart(user)
+        return askifrestart.restart()
 
     def visitor3_attractions(self):
-        pass
+        print("Jurassic Park has many options for its visitors\nPlease find below some of them\n1.Park Ride\nLab Cinema with Mr DNA\nHelicopter Ride\nAbove ride with Jurassic Monorail\nMuseum\nMany others! Get our Booklet in the Visitor Center and explore all options of Jurassic Park!\n")
 
-    def visitor_Execucao(self):
-        pass #fazer um IF q vai puxar o DEF de acordo com a variavel choice do MainMenu
+        askifrestart = Restart(user)
+        return askifrestart.restart()
+
+    def visitor_execucao(self):
+        if self.choice == 1:
+            return self.visitor1_knowthepark()
+        elif self.choice ==2:
+            return self.visitor2_dinosaurcatalogue()
+        else:
+            return self.visitor3_attractions()
 
 
 class EmployeeMenu: 
@@ -93,27 +110,3 @@ class Restart: #OK
 
         else:
             return "Thank you for using Jurassic Park's interactive system"
-
-class ClasseAserDescartada:
-
-    def choice1_knowthepark(self):
-        print("The park is divided in 4 main areas. The carnivores are located at the east and west zones, whereas the herbivores are in the North and South.\nThe Helicopter ride is located at the South shore.\nThe Port is at the East shore.\nThe Visitor center is at the West island.\nYou can get a map with all locations and attractions at the Visitor Center.\n")
-
-        
-
-
-
-class DinoCatalogue:
-
-    #print(The {dino, list[0]} is a {type, list[1]} from {period, list[2] period}. 
-    #It's a {eating} that used to live in {habitat}. {impacttext})
-    pass
-
-class Atractions:
-    #fazer catalogo com atracoes, com link com os dinosauros disponiveis em cada uma
-    pass
-
-class DNALab:
-    #fazer classe privada
-
-
